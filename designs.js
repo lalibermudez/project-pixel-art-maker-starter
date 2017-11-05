e5fcbefcd42c6490df09c2470f882f10b9fdcbf1
Lali Bermudez <laliberbello@gmail.com>
2017-11-05 09:47:37 -0500

    Create Height and Width variables

--- designs.js
+++ designs.js
@@ -1,10 +1,13 @@
 // Select color input
 // Select size input
+let gridHeight = $('#input_height').val();
+let gridWidth = $('#input_width').val();
 
 // When size is submitted by the user, call makeGrid()
 
 
 $('#sizePicker').submit(function makeGrid() {
+	
 
 // Your code goes here!
 
