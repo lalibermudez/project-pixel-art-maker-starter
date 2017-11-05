1515a8ed46ea28af4b9ecc9bd6ae8885b3dce210
Lali Bermudez <laliberbello@gmail.com>
2017-11-05 13:40:34 -0500

    Get Height and Width values

--- designs.js
+++ designs.js
@@ -1,13 +1,23 @@
 // Select color input
 // Select size input
-let gridHeight = $('#input_height').val();
-let gridWidth = $('#input_width').val();
+let gridHeight, gridWidth;
+
+$('#input_height').keyup(function () {
+	gridHeight = $(this).val();
+	$(this).attr('value', gridHeight);
+});
+
+$('#input_width').keyup(function () {
+	gridWidth = $(this).val();
+	$(this).attr('value', gridWidth);
+});
+
 
 // When size is submitted by the user, call makeGrid()
 
 
 $('#sizePicker').submit(function makeGrid() {
-	
+
 
 // Your code goes here!
 
