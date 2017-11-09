25fdc828355a635402a23aaaaad9520b64ecee6c
Lali Bermudez <laliberbello@gmail.com>
2017-11-08 21:21:23 -0500

    Add Udacity review suggestions

--- designs.js
+++ designs.js
@@ -7,25 +7,15 @@ $('#colorPicker').change(function colorSelect() {
 	$(this).attr('value', color);
 });
 
-// Select size input
-
-let gridHeight, gridWidth;
-
-$('#input_height').change(function () {
-	gridHeight = $(this).val();
-	$(this).attr('value', gridHeight);
-});
-
-$('#input_width').change(function () {
-	gridWidth = $(this).val();
-	$(this).attr('value', gridWidth);
-});
-
-
 // When size is submitted by the user, call makeGrid()
 
 $('#sizePicker').submit(function makeGrid() {
 	$('tr').remove();
+
+	// Select size input
+	let gridHeight = $('#input_height').val();
+	let gridWidth = $('#input_width').val();
+
 	for (let i = 1; i <= gridHeight; i++) {
 		$('table').append('<tr></tr>');
 	};
@@ -33,8 +23,7 @@ $('#sizePicker').submit(function makeGrid() {
 		$('tr').append('<td></td>');
 	};
 	
-	// When cells are click change to selected color
-
+	// When cells are clicked change to selected color
 	$('td').click(function () {
 		$(this).css('background-color', color);
 	});
